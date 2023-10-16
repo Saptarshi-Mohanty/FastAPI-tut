93fa67152a0df446b0831444c213c53dbe90f966
saptarshi <saptarshi@assemblyline.store>
2023-10-16 15:30:20 +0530

    Field pydantic

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     name: str
@@ -7,5 +7,6 @@ class Item(BaseModel):
     tax: float|None=None
 
 class User(BaseModel):
-    username_first: str
-    username_last: str
+    username_first: str| None = Field(None, title="First Name", max_length=20)
+    username_last: str| None = Field(..., title="Last Name", max_length=25)
+    
