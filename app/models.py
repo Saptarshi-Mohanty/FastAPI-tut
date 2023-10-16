c0a94c0df2424dad7e661b703e509939a5e951e6
saptarshi <saptarshi@assemblyline.store>
2023-10-16 16:59:30 +0530

    nested models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
+
+class Image(BaseModel):
+    name: str
+    description: str
+    url : HttpUrl
 
 class Item(BaseModel):
     name: str
     descrpition: str|None=None
     price: float
     tax: float|None=None
+    image: Image
 
 class User(BaseModel):
     username_first: str| None = Field(None, title="First Name", max_length=20)
     username_last: str| None = Field(..., title="Last Name", max_length=25)
-    
